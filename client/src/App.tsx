2391a1c557a5036023223c9286238494225ce079
Mariano Encalada <projectcodingzero@gmail.com>
2026-09-18 00:05:15 -0300

    feat(deno): Cambio de node a deno - Se empieza con deno build - Tiene deno task build,dev,preview, lint y start

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { AppLayout } from './layouts/AppLayout'
-import { PatientsListPage } from './features/patients/PatientsListPage'
-import { PatientDetailPage } from './features/patients/PatientDetailPage'
-import { SpecialistsListPage } from './features/specialists/SpecialistsListPage'
-import { AppointmentsPage } from './features/appointments/AppointmentsPage'
-import { InventoryPage } from './features/inventory/InventoryPage'
-import { OwnersListPage } from './features/owners/OwnersListPage'
+import { AppLayout } from './layouts/AppLayout.tsx'
+import { PatientsListPage } from './features/patients/PatientsListPage.tsx'
+import { PatientDetailPage } from './features/patients/PatientDetailPage.tsx'
+import { SpecialistsListPage } from './features/specialists/SpecialistsListPage.tsx'
+import { AppointmentsPage } from './features/appointments/AppointmentsPage.tsx'
+import { InventoryPage } from './features/inventory/InventoryPage.tsx'
+import { OwnersListPage } from './features/owners/OwnersListPage.tsx'
 
 const queryClient = new QueryClient()
 
